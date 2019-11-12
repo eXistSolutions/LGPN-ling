5b0cd17fa14d8d8477e51a1612a2150431288c88
joern@existsolutions.com <9az.xmJWY_d~>
2019-11-12 15:21:25 +0100

    removed bower dependency: just managing those 2 deps by putting them into 'resources'; fixed gulpfile - does only handle CSS building from LESS; deploy and watch task should also still work; updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,239 +1,179 @@
 'use strict';
 
-var fs =                    require('fs'),
-    gulp =                  require('gulp'),
-    exist =                 require('gulp-exist'),
-    newer =                 require('gulp-newer'),
-    del =                   require('del'),
-    less =                  require('gulp-less'),
-    path =                  require('path'),
-    sourcemaps =            require('gulp-sourcemaps'),
-    LessAutoprefix =        require('less-plugin-autoprefix'),
-    autoprefix =            new LessAutoprefix({ browsers: ['last 2 versions'] }),
-    LessPluginCleanCSS =    require('less-plugin-clean-css'),
-    cleanCSSPlugin =        new LessPluginCleanCSS({advanced: true}),
+// TODO: 1. Refactor tasks with named logic functions to avoid anonymous functions
+// https://github.com/gulpjs/gulp/issues/1392#issuecomment-156789791
+// 2. Declare input and output objects for better maintenance
+
+const fs = require('fs'),
+    gulp = require('gulp'),
+    exist = require('@existdb/gulp-exist'),
+    watch = require('gulp-watch'),
+    less = require('gulp-less'),
+    sourcemaps = require('gulp-sourcemaps'),
+    path = require('path'),
+    del = require('del'),
+    LessAutoprefix = require('less-plugin-autoprefix'),
+    autoprefixer = new LessAutoprefix({ browsers: ['last 2 versions'] });
 
 
-    input = {
-        'html':             ['*.html', '*.xhtml'],
-        'templates':         'templates/**/*.html',
-        'css':               'resources/css/less/style.less',
-        'scripts':           'resources/js/**/*',
-        'vendor_scripts':    [
-                                'bower_components/jquery/dist/jquery.min.js',
-                                'bower_components/bootstrap/dist/js/bootstrap.min.js'
-                             ],
-        'xml':               'resources/xml/*.xml',
-        'images':            'resources/img/**/*',
-        'fonts':             'bower_components/bootstrap/fonts/**/*',
-        'articles':          'data/articles/*.html',
-        'i18n':              'data/i18n/*.xml'
-    },
-    output  = {
-        'html':              '.',
-        'templates':         'templates',
-        'css':               'resources/css',
-        'vendor_css':        'resources/css',
-        'scripts':           'resources/js',
-        'vendor_scripts':    'resources/js/vendor',
-        'xml':               'resources/xml',
-        'images':            'resources/img',
-        'fonts':             'resources/fonts',
-        'articles':          'data/articles',
-        'i18n':              'data/i18n'
-    }
-    ;
+// Deployment //
 
-// *************  existDB configuration *************** //
+let localConnectionOptions = {};
 
-// var localConnectionOptions = {};
-//
-// if (fs.existsSync('./local.node-exist.json')) {
-//     localConnectionOptions = require('./local.node-exist.json');
-//     console.log('read from localConnectionOptions', localConnectionOptions)
-// }
-//
-// var exClient = exist.createClient(localConnectionOptions);
-//
-// var targetConfiguration = {
-//     target: '/db/apps/lgpn-ling/'
-// };
+if (fs.existsSync('./local.node-exist.json')) {
+    localConnectionOptions = require('./local.node-exist.json');
+    console.log('read from localConnectionOptions', localConnectionOptions)
+}
 
-exist.defineMimeTypes({
-    'application/xml': ['odd']
+let exClient = exist.createClient(localConnectionOptions);
+
+/* create a file named 'local.node-exist.json' with the following content:
+{
+  "host": "localhost",
+  "port": "8080",
+  "path": "/exist/xmlrpc",
+  "basic_auth": {
+    "user": "admin",
+    "pass": ""
+  }
+}
+*/
+
+let targetConfiguration = { target: '/db/apps/lgpn-ling/' }
+
+gulp.task('clean', function() {
+    return del([
+        'build/**/*',
+        'resources/css/style.css',
+        'resources/fonts/*'
+    ]);
 });
 
-var exClient = exist.createClient({
-    host: 'localhost',
-    port: '8080',
-    path: '/exist/xmlrpc',
-    basic_auth: { user: 'admin', pass: '' }
+
+// Scripts //
+
+gulp.task('scripts:deploy', function () {
+    return gulp.src('resources/scripts/*', {base: '.'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
 });
 
-var targetConfiguration = {
-    target: '/db/apps/lgpn-ling/',
-    html5AsBinary: true
-};
-
-// ****************  Styles ****************** //
-
-gulp.task('build:styles', function(){
-    return gulp.src(input.css)
-        .pipe(sourcemaps.init())
-        .pipe(less({ plugins: [cleanCSSPlugin, autoprefix] }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(output.css))
+gulp.task('scripts:watch', function () {
+    gulp.watch('resources/scripts/**/*.js', gulp.series('scripts:deploy'))
 });
 
-gulp.task('deploy:styles', ['build:styles'], function () {
-    console.log('deploying less and css files');
-    return gulp.src('resources/css/**/*', {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
+
+// Styles //
+
+gulp.task('styles:build', gulp.series('clean', function () {
+    let compiler = less({
+        paths: [
+            path.join(__dirname, "resources/js/vendor/bootstrap/less"),
+            path.join(__dirname, "resources/css/less")
+        ],
+        plugins: [
+            autoprefixer
+        ]
+    })
+    return gulp.src('resources/css/less/style.less')
+    .pipe(sourcemaps.init({largeFile: true}))
+    .pipe(compiler)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('resources/css'))
+}));
+0
+gulp.task('styles:deploy', gulp.series('styles:build', function () {
+    return gulp.src('resources/css/*.css', {base: './'})
+    .pipe(exClient.dest(targetConfiguration))
+}));
+
+gulp.task('styles:watch', function () {
+    gulp.watch('resources/css/less/**/*.less', gulp.series('styles:deploy'))
 });
 
-gulp.task('watch:styles', function () {
-    console.log('watching less files');
-    gulp.watch('resources/css/less/**/*.less', ['deploy:styles'])
+
+// Pages //
+
+let pagesPath = '*.html';
+gulp.task('pages:deploy', function () {
+    return gulp.src(pagesPath, {base: './'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
 });
 
-// ****************  Fonts ****************** //
-
-gulp.task('fonts:copy', function () {
-    return gulp.src(input.fonts)
-        .pipe(gulp.dest(output.fonts))
+gulp.task('pages:watch', function () {
+    gulp.watch(pagesPath, gulp.series('pages:deploy'))
 });
 
-gulp.task('fonts:deploy', ['fonts:copy'], function () {
-    return gulp.src(output.fonts, {base: '.'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
+// Templates //
+
+let templatesPath = 'templates/**/*.html';
+gulp.task('templates:deploy', function () {
+    return gulp.src(templatesPath, {base: './'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
 });
 
-// ****************  Scripts ****************** //
-
-gulp.task('vendor_scripts:copy', function () {
-    return gulp.src(input.vendor_scripts)
-        .pipe(gulp.dest(output.vendor_scripts));
+gulp.task('templates:watch', function () {
+    gulp.watch(templatesPath, gulp.series('templates:deploy'))
 });
 
-gulp.task('deploy:scripts', ['vendor_scripts:copy'], function () {
-    return gulp.src(input.scripts, {base: '.'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
+// Modules //
+
+let modulesPath = 'modules/**/*';
+gulp.task('modules:deploy', function () {
+    return gulp.src(modulesPath, {base: './'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
 });
 
-// Watch scripts
-gulp.task('watch:scripts', function () {
-    gulp.watch(input.scripts, ['deploy:scripts'])
+gulp.task('modules:watch', function () {
+    gulp.watch(modulesPath, gulp.series('modules:deploy'))
 });
 
-// *************  Templates *************** //
+// Tests //
 
-// Deploy templates
-gulp.task('deploy:templates', function () {
-    return gulp.src(input.templates, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
+let testPath = 'tests/**/*';
+gulp.task('deploy:test', function () {
+    return gulp.src(testPath, {base: './'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
 });
 
-// Watch templates
-gulp.task('watch:templates', function () {
-    gulp.watch(input.templates, ['deploy:templates'])
+gulp.task('watch:test', function () {
+    gulp.watch(testPath, gulp.series('deploy:test'))
 });
 
-// *************  HTML Pages *************** //
+// *************  Files in project root *************** //
 
-// Deploy HTML pages
-gulp.task('deploy:html', function () {
-    return gulp.src(input.html, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
+let otherPath = '*{.xpr,.xqr,.xql,.xml,.xconf,.js}';
+
+gulp.task('other:deploy', function () {
+    return gulp.src(otherPath, {base: './'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
 });
 
-// Watch HTML pages
-gulp.task('watch:html', function () {
-    gulp.watch(input.html, ['deploy:html'])
+gulp.task('watch:other', function () {
+    gulp.watch(otherPath, gulp.series('other:deploy'))
 });
 
-// *************  XML Pages *************** //
 
-gulp.task('deploy:xml', function () {
-    return gulp.src(input.xml, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-});
+// General //
 
-// Watch xml files
-gulp.task('watch:xml', function () {
-   gulp.watch(input.xml, ['deploy:xml'])
-});
+gulp.task('build', gulp.series('styles:build'));
 
-// *************  Articles, i18n keys *************** //
+gulp.task('deploy', gulp.series('build', function () {
+    return gulp.src([
+        'resources/**/*',
+        pagesPath,
+        modulesPath,
+        templatesPath
+    ], {base: './'})
+    .pipe(exClient.newer(targetConfiguration))
+    .pipe(exClient.dest(targetConfiguration))
+}));
 
-gulp.task('deploy:articles', function () {
-    return gulp.src(input.articles, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-});
+gulp.task('watch', gulp.series('deploy', gulp.parallel('styles:watch', 'pages:watch', 'templates:watch', 'scripts:watch', 'modules:watch', 'watch:other')));
 
-// Watch articles
-gulp.task('watch:articles', function () {
-   gulp.watch(input.articles, ['deploy:articles'])
-});
-
-gulp.task('deploy:i18n', function () {
-    return gulp.src(input.i18n, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-});
-
-// Watch i18n
-gulp.task('watch:i18n', function () {
-   gulp.watch(input.i18n, ['deploy:i18n'])
-});
-
-// *************  Images *************** //
-
-gulp.task('deploy:images', function () {
-    return gulp.src(input.articles, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-});
-
-// Watch articles
-gulp.task('watch:images', function () {
-   gulp.watch(input.images, ['deploy:images'])
-});
-
-// *************  General Tasks *************** //
-
-var pathsToWatchAndDeploy = [
-    'templates/**/*.html',
-    'resources/**/*',
-    'transform/*',
-    '*.html',
-    '*.xhtml',
-    '*{.xpr,.xqr,.xql,.xml,.xconf}',
-    'modules/**/*',
-    '!build.*',
-    'data/**/*',
-    'img/*'
-];
-
-// Watch and deploy all changed files
-gulp.task('watch', ['watch:html', 'watch:styles', 'watch:scripts', 'watch:xml', 'watch:i18n', 'watch:articles', 'watch:images', 'watch:templates']);
-
-gulp.task('build', ['build:styles', 'fonts:copy', 'vendor_scripts:copy']);
-
-// Deploy files to existDB
-gulp.task('deploy', ['build'], function () {
-    console.log('deploying files to local existdb');
-    return gulp.src(pathsToWatchAndDeploy, {base: './'})
-        .pipe(exClient.newer(targetConfiguration))
-        .pipe(exClient.dest(targetConfiguration))
-});
-
-// Default task (which is called by 'npm start' task)
-gulp.task('default', ['build']);
-
+gulp.task('default', gulp.series('build'));
